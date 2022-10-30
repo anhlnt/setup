60dddf8b0392c622669eeebee977f0cc31406772
Le Nang Tuan Anh <31811207+anhlnt@users.noreply.github.com>
2022-10-30 22:26:32 +0900

    Update setup_zsh_ubuntu.sh

--- setup_zsh_ubuntu.sh
+++ setup_zsh_ubuntu.sh
@@ -4,6 +4,6 @@ chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
-echo '# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh. \n\
+echo '# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh. \n
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' >>~/.zshrc
 cp .p10k.zsh ~/ 
